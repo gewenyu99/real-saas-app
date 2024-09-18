ba07bc502f03abfdff65684075c3bf073d5f10d9
Vincent (Wen Yu) Ge <gewenyu99@gmail.com>
2024-09-18 21:14:54 +0000

    Convert repo to a fake saas repo

--- apple/swift/Tests/MyCLITests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-
-@Suite("best tests evar") struct MyCoolTests {
-    @Test("rad test name") func helloworld() {
-        let greeting = "hello, world!"
-        #expect(greeting == "hello, worldzx!")
-    }
-}
